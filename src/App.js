289093a55f17f1d9b8f268765bf2045c2655d814
unknown <ramib1207@gmail.com>
2022-06-24 16:12:46 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {Link, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <div>
+        <nav>
+          <h1>Lambda Eats</h1>
+          <div>  
+            <Link to="/">Home</Link>
+            <Link to="/pizza">Pizza?</Link>
+          </div>
+        </nav>
+      </div>
     </>
   );
 };
